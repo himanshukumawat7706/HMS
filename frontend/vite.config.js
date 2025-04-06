257e1eb188030aa36272dbd26d69d3e604e69352
himanshukumawat7706 <your-email@example.com>
2025-04-06 21:02:12 +0530

    commit

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,5 +4,8 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.VITE_BASE_PATH 
+  base: process.env.VITE_BASE_PATH ,
+  build: {
+    chunkSizeWarningLimit: 2000, // increase from 500 kB
+  },
 })
